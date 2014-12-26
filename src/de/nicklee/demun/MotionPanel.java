a1ff9849e7383b50a9b90be1920049912a93d09d
Nick Lee <email@nickle.es>
2014-12-26 13:35:41 -0500

    Added more tabs to motion builder interface

--- src/de/nicklee/demun/MotionPanel.java
+++ src/de/nicklee/demun/MotionPanel.java
@@ -20,6 +20,8 @@ public class MotionPanel extends JPanel {
 	private JTextField textField;
 	private JTextField textField_1;
 	private JTextField textField_2;
+	private JTextField txtDoctitlefield;
+	private JTextField txtMainsponsorfield;
 
 	/**
 	 * Create the panel.
@@ -27,12 +29,95 @@ public class MotionPanel extends JPanel {
 	public MotionPanel() {
 		setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
 		
-		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.LEFT);
-		tabbedPane.setTabLayoutPolicy(JTabbedPane.SCROLL_TAB_LAYOUT);
-		add(tabbedPane);
+		JTabbedPane motionBuilder = new JTabbedPane(JTabbedPane.LEFT);
+		add(motionBuilder);
+		
+		JPanel introDoc = new JPanel();
+		motionBuilder.addTab("Intro", null, introDoc, null);
+		GridBagLayout gbl_introDoc = new GridBagLayout();
+		gbl_introDoc.columnWidths = new int[]{0, 0, 0, 0, 0};
+		gbl_introDoc.rowHeights = new int[]{0, 0, 0, 0, 0};
+		gbl_introDoc.columnWeights = new double[]{1.0, 0.0, 1.0, 1.0, Double.MIN_VALUE};
+		gbl_introDoc.rowWeights = new double[]{1.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
+		introDoc.setLayout(gbl_introDoc);
+		
+		JLabel lblNewLabel_7 = new JLabel("<html><b>Document Title:</b></html>");
+		GridBagConstraints gbc_lblNewLabel_7 = new GridBagConstraints();
+		gbc_lblNewLabel_7.anchor = GridBagConstraints.EAST;
+		gbc_lblNewLabel_7.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_7.gridx = 1;
+		gbc_lblNewLabel_7.gridy = 1;
+		introDoc.add(lblNewLabel_7, gbc_lblNewLabel_7);
+		
+		txtDoctitlefield = new JTextField();
+		txtDoctitlefield.setText("docTitleField");
+		GridBagConstraints gbc_txtDoctitlefield = new GridBagConstraints();
+		gbc_txtDoctitlefield.insets = new Insets(0, 0, 5, 5);
+		gbc_txtDoctitlefield.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txtDoctitlefield.gridx = 2;
+		gbc_txtDoctitlefield.gridy = 1;
+		introDoc.add(txtDoctitlefield, gbc_txtDoctitlefield);
+		txtDoctitlefield.setColumns(10);
+		
+		JLabel lblNewLabel_8 = new JLabel("<html><b>Main Sponsor:</b></html>");
+		GridBagConstraints gbc_lblNewLabel_8 = new GridBagConstraints();
+		gbc_lblNewLabel_8.anchor = GridBagConstraints.EAST;
+		gbc_lblNewLabel_8.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_8.gridx = 1;
+		gbc_lblNewLabel_8.gridy = 2;
+		introDoc.add(lblNewLabel_8, gbc_lblNewLabel_8);
+		
+		txtMainsponsorfield = new JTextField();
+		txtMainsponsorfield.setText("mainSponsorField");
+		GridBagConstraints gbc_txtMainsponsorfield = new GridBagConstraints();
+		gbc_txtMainsponsorfield.insets = new Insets(0, 0, 5, 5);
+		gbc_txtMainsponsorfield.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txtMainsponsorfield.gridx = 2;
+		gbc_txtMainsponsorfield.gridy = 2;
+		introDoc.add(txtMainsponsorfield, gbc_txtMainsponsorfield);
+		txtMainsponsorfield.setColumns(10);
+		
+		JPanel closeTopic = new JPanel();
+		motionBuilder.addTab("Close", null, closeTopic, null);
+		GridBagLayout gbl_closeTopic = new GridBagLayout();
+		gbl_closeTopic.columnWidths = new int[]{0, 0, 0, 0, 0};
+		gbl_closeTopic.rowHeights = new int[]{0, 0, 0, 0, 0};
+		gbl_closeTopic.columnWeights = new double[]{1.0, 0.0, 1.0, 1.0, Double.MIN_VALUE};
+		gbl_closeTopic.rowWeights = new double[]{1.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
+		closeTopic.setLayout(gbl_closeTopic);
+		
+		JLabel lblNewLabel_4 = new JLabel("<html><b>Type:<b></html>");
+		GridBagConstraints gbc_lblNewLabel_4 = new GridBagConstraints();
+		gbc_lblNewLabel_4.anchor = GridBagConstraints.EAST;
+		gbc_lblNewLabel_4.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_4.gridx = 1;
+		gbc_lblNewLabel_4.gridy = 1;
+		closeTopic.add(lblNewLabel_4, gbc_lblNewLabel_4);
+		
+		JLabel lblNewLabel_5 = new JLabel("Dynamic");
+		GridBagConstraints gbc_lblNewLabel_5 = new GridBagConstraints();
+		gbc_lblNewLabel_5.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_5.gridx = 2;
+		gbc_lblNewLabel_5.gridy = 1;
+		closeTopic.add(lblNewLabel_5, gbc_lblNewLabel_5);
+		
+		JLabel lblTopic = new JLabel("<html><b>Topic:<b></html>");
+		GridBagConstraints gbc_lblTopic = new GridBagConstraints();
+		gbc_lblTopic.anchor = GridBagConstraints.EAST;
+		gbc_lblTopic.insets = new Insets(0, 0, 5, 5);
+		gbc_lblTopic.gridx = 1;
+		gbc_lblTopic.gridy = 2;
+		closeTopic.add(lblTopic, gbc_lblTopic);
+		
+		JLabel lblNewLabel_6 = new JLabel("Dynamic");
+		GridBagConstraints gbc_lblNewLabel_6 = new GridBagConstraints();
+		gbc_lblNewLabel_6.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_6.gridx = 2;
+		gbc_lblNewLabel_6.gridy = 2;
+		closeTopic.add(lblNewLabel_6, gbc_lblNewLabel_6);
 		
 		JPanel unmodCaucus = new JPanel();
-		tabbedPane.addTab("Unmod. Caucus", null, unmodCaucus, null);
+		motionBuilder.addTab("Unmod", null, unmodCaucus, null);
 		GridBagLayout gbl_unmodCaucus = new GridBagLayout();
 		gbl_unmodCaucus.columnWidths = new int[]{0, 0, 0, 0, 0, 0};
 		gbl_unmodCaucus.rowHeights = new int[]{0, 0, 0, 0, 0};
@@ -91,7 +176,7 @@ public class MotionPanel extends JPanel {
 		unmodCaucus.add(textField_1, gbc_textField_1);
 		
 		JPanel modCaucus = new JPanel();
-		tabbedPane.addTab("Moderated Caucus", null, modCaucus, null);
+		motionBuilder.addTab("Moderated", null, modCaucus, null);
 		GridBagLayout gbl_modCaucus = new GridBagLayout();
 		gbl_modCaucus.columnWidths = new int[]{0, 0, 0, 0, 0, 0};
 		gbl_modCaucus.rowHeights = new int[]{0, 0, 0, 0, 0, 0};
@@ -184,14 +269,14 @@ public class MotionPanel extends JPanel {
 		modCaucus.add(textField, gbc_textField);
 		textField.setColumns(10);
 		
-		JPanel MotionQueue = new JPanel();
-		add(MotionQueue);
-		GridBagLayout gbl_MotionQueue = new GridBagLayout();
-		gbl_MotionQueue.columnWidths = new int[]{130, 130, 130, 0};
-		gbl_MotionQueue.rowHeights = new int[]{34, 50, 50, 0};
-		gbl_MotionQueue.columnWeights = new double[]{1.0, 0.0, 1.0, Double.MIN_VALUE};
-		gbl_MotionQueue.rowWeights = new double[]{1.0, 0.0, 0.0, Double.MIN_VALUE};
-		MotionQueue.setLayout(gbl_MotionQueue);
+		JPanel motionQueue = new JPanel();
+		add(motionQueue);
+		GridBagLayout gbl_motionQueue = new GridBagLayout();
+		gbl_motionQueue.columnWidths = new int[]{130, 130, 130, 0};
+		gbl_motionQueue.rowHeights = new int[]{34, 50, 50, 0};
+		gbl_motionQueue.columnWeights = new double[]{1.0, 0.0, 1.0, Double.MIN_VALUE};
+		gbl_motionQueue.rowWeights = new double[]{1.0, 0.0, 0.0, Double.MIN_VALUE};
+		motionQueue.setLayout(gbl_motionQueue);
 		
 		JScrollPane scrollPane = new JScrollPane();
 		GridBagConstraints gbc_scrollPane = new GridBagConstraints();
@@ -200,7 +285,7 @@ public class MotionPanel extends JPanel {
 		gbc_scrollPane.fill = GridBagConstraints.BOTH;
 		gbc_scrollPane.gridx = 0;
 		gbc_scrollPane.gridy = 0;
-		MotionQueue.add(scrollPane, gbc_scrollPane);
+		motionQueue.add(scrollPane, gbc_scrollPane);
 		
 		JList list = new JList();
 		scrollPane.setViewportView(list);
@@ -221,7 +306,7 @@ public class MotionPanel extends JPanel {
 		gbc_btnNewButton.insets = new Insets(0, 0, 5, 0);
 		gbc_btnNewButton.gridx = 0;
 		gbc_btnNewButton.gridy = 1;
-		MotionQueue.add(btnNewButton, gbc_btnNewButton);
+		motionQueue.add(btnNewButton, gbc_btnNewButton);
 		
 		JButton btnNewButton_1 = new JButton("Delete Motion");
 		GridBagConstraints gbc_btnNewButton_1 = new GridBagConstraints();
@@ -229,7 +314,7 @@ public class MotionPanel extends JPanel {
 		gbc_btnNewButton_1.fill = GridBagConstraints.BOTH;
 		gbc_btnNewButton_1.gridx = 0;
 		gbc_btnNewButton_1.gridy = 2;
-		MotionQueue.add(btnNewButton_1, gbc_btnNewButton_1);
+		motionQueue.add(btnNewButton_1, gbc_btnNewButton_1);
 		
 		JButton btnNewButton_2 = new JButton("Move Up");
 		GridBagConstraints gbc_btnNewButton_2 = new GridBagConstraints();
@@ -237,14 +322,14 @@ public class MotionPanel extends JPanel {
 		gbc_btnNewButton_2.insets = new Insets(0, 0, 0, 5);
 		gbc_btnNewButton_2.gridx = 1;
 		gbc_btnNewButton_2.gridy = 2;
-		MotionQueue.add(btnNewButton_2, gbc_btnNewButton_2);
+		motionQueue.add(btnNewButton_2, gbc_btnNewButton_2);
 		
 		JButton btnNewButton_3 = new JButton("Move Down");
 		GridBagConstraints gbc_btnNewButton_3 = new GridBagConstraints();
 		gbc_btnNewButton_3.fill = GridBagConstraints.BOTH;
 		gbc_btnNewButton_3.gridx = 2;
 		gbc_btnNewButton_3.gridy = 2;
-		MotionQueue.add(btnNewButton_3, gbc_btnNewButton_3);
+		motionQueue.add(btnNewButton_3, gbc_btnNewButton_3);
 		
 		JPanel passFailPanel = new JPanel();
 		add(passFailPanel);
